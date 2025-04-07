04591bfaf13fd37497b93b582cdb7731012070f5
ousmanediallo826 <ousmane.diallo3@mail.citytech.cuny.edu>
2025-04-07 02:55:32 -0400

    changing deps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  reactStrictMode: true, // Enables React Strict Mode for additional debugging
+  experimental: {
+    appDir: true, // Enable App Directory feature
+  },
 };
-
-export default nextConfig;
